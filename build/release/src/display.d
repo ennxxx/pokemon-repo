0331df3a955e42329053c35bdc7ec52a511828a6
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2022-06-04 11:41:56 +0800

    Build file

---
+++ build/release/src/display.d
@@ -0,0 +1,8 @@
+build/release/src/display.o: src/display.c src/../include/manageMenu.h \
+  include/pokedexTools.h src/../include/pokedexTools.h
+
+src/../include/manageMenu.h:
+
+include/pokedexTools.h:
+
+src/../include/pokedexTools.h:
