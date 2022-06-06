3e8df6f209920ca122fed17a8c3ddf94e9b0b34a
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2022-06-06 08:02:22 +0800

    Implemented delete function
    There is a switch case here to test if it displays properly. Remove it after testing.
    
    Issue:
    Deleting consecutively will cause issues with the display. Will fix this later.

---
+++ src/delete.c
@@ -0,0 +1,47 @@
+// Deletes entries
+
+#include <stdio.h>
+#include "../include/manageMenu.h"
+#include "../include/pokedexTools.h"
+
+void deleteEntry(Pokedex* dex)
+{
+    int exists, entry, entryIndex;
+    int i, back;
+    
+    exists = entryIndex = entry = -1;
+
+    while (exists == -1)
+    {
+        printf("\nWhich index would you like to delete?: ");
+        scanf("%d", &entry);
+        exists = entryExists(*dex, entry);
+        if (exists == -1)
+        {
+            printf("\nThat entry does not exist.\n"); 
+        }
+    }
+
+    if (exists != -1)
+    {
+        entryIndex = exists;
+    }
+
+    // Deletes based on index
+    
+    for (i = entry - 1; i < dex->pokeCount - 1; i++)  
+    {  
+        dex->collection[i] = dex->collection[i + 1]; 
+        dex->collection[i].entry -= 1;  // This updates the entry number
+    }  
+    
+    printf("\nPokemon Entry %d has been DELETED. Press [0] to return: ", entry + 1);
+    scanf("%d", &back);
+
+    switch(back)
+    {
+        case 0: manageMenu(*dex); break;
+        case 1: break;
+        default: break;
+    }
+}
