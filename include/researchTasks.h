4bd15763af9876377ace18a448905c48b5bb99c9
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2022-06-11 14:41:41 +0800

    Updated researchTasks.h
    Added comments to explain the functions.

--- include/researchTasks.h
+++ include/researchTasks.h
@@ -3,20 +3,22 @@
 
 #include "pokedexTools.h"
 
-//Function that displays all pokemon entries and their research task status
+//Function that displays all Pokemon entries and their research task status
 void reviewTasksByPokemon(Pokedex dex);
 
 //Function that displays the the research tasks per type
 void reviewTasksByType(Pokedex dex);
 
-//Function that updates the research tasks per pokemon
+//Function that updates the research tasks per Pokemon
 void updateTasks(Pokedex* dex);
 
-//Function that sorts the pokemon according to research tasks completion
+// Function that checks the progress of research tasks per Pokemon
+void updateProgress(Pokemon* mon);
+
+// Function that sorts the Pokemon according to research tasks completion
 void rankPokemon(Pokedex* dex);
 
+// Function that allows a user to add more task types
 void addTaskTypes(Pokedex* dex);
-
-void updateProgress(Pokemon* mon);
 
 #endif
