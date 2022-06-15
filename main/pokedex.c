047c17554178f43ab86a6251b30ac06d69bc9191
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2022-06-15 15:08:54 +0800

    Added the Code of Honesty Clause

--- main/pokedex.c
+++ main/pokedex.c
@@ -1,3 +1,15 @@
+/*
+ * This is to certify that this project is our own work, based on our personal
+ * efforts in studying and applying the concepts learned. We have constructed
+ * the functions and their respective algorithms and corresponding code by
+ * ourselves. The program was run, tested, and debugged by our own efforts.
+ * We further certify that we have not copied in part or whole or otherwise
+ * plagiarized the work of other students and/or persons.
+ *
+ * AEINNOR A. REYES, 121113542
+ * PAULA ENXI D.V. PACHECO, 12133655
+ */
+
 #include "../include/manageMenu.h"
 #include "../include/pokedexTools.h"
 #include "../include/researchTasks.h"
